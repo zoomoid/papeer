d74915b0d8233ab1cb19a1e3e7f0da7a17769312
Alexander Bartolomey <occloxium@gmail.com>
2021-06-27 13:29:19 +0200

    refactor: cmd module entrypoint
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- cmd/papeer/papeer.go
+++ cmd/papeer/papeer.go
@@ -1,54 +1,153 @@
 package main
 
 import (
-	"flag"
+	"errors"
+	"io/fs"
+	"os"
 
-	"go.uber.org/zap"
-
+	flag "github.com/spf13/pflag"
 	"github.com/zoomoid/papeer/pkg/runner"
-	types "github.com/zoomoid/papeer/pkg/types"
+	"github.com/zoomoid/papeer/pkg/types"
+	"go.uber.org/zap"
 )
 
-func main() {
+var watchArg bool
+var fileArg string
+var directoryArg string
+var landscapeArg bool
+var scaleArg float32
+var pagesArg string
+var gridArg bool
+var gridOpacityArg float32
+var gridColorArg string
+var gridScaleArg float32
+var formatArg string
+var deltaArg []string
+
+var supportedFormats = []string{
+	"a2paper",
+	"a3paper",
+	"a4paper",
+}
+
+func main() int {
+	flag.BoolVarP(&watchArg, "watch", "w", false, "Start in watch mode with directory")
+	flag.StringVarP(&fileArg, "file", "f", "", "Explicit filename given")
+	flag.StringVarP(&directoryArg, "directory", "d", "", "Explicit filename given")
+	flag.BoolVar(&landscapeArg, "landscape", true, "Wrap the pdf in a landscape format")
+	flag.Float32Var(&scaleArg, "scale", 0.9, "Paper scale on the new page")
+	flag.StringVar(&pagesArg, "pages", "-", "pdfpages-compliant 'pages' parameter. '-' means all pages of the given pdf")
+	flag.BoolVar(&gridArg, "grid", true, "Add a dotted grid with 5mm spacing to the background")
+	flag.Float32Var(&gridOpacityArg, "grid-opacity", 0.1, "Specify the opacity of the grid")
+	flag.StringVar(&gridColorArg, "grid-color", "black!30", "Specify the color of the grid dots")
+	flag.Float32Var(&gridScaleArg, "grid-scale", 1.0, "Specify the scale of the grid")
+	flag.StringVar(&formatArg, "format", "a3paper", "Paper format to embed the pdf in")
+	flag.StringSliceVar(&deltaArg, "delta", []string{"10cm", "10cm"}, "Specify the page margins in (x,y) order")
+
+	flag.Parse()
+
+	filehandle := flag.Arg(0)
+
 	logger, _ := zap.NewProduction()
 	defer logger.Sync()
 
-	landscapeArg := flag.Bool("landscape", true, "Wrap the pdf in a landscape format")
-	gridArg := flag.Bool("grid", true, "Add a dotted grid with 5mm spacing to the background")
-	formatArg := flag.String("format", "a3paper", "Paper format to embed the pdf in")
-	flag.Parse()
-
-	fileArg := flag.Arg(0)
-
-	logger.Info("Starting papeer",
-		zap.String("file", fileArg),
-		zap.Bool("landscape", *landscapeArg),
-		zap.Bool("grid", *gridArg),
-		zap.String("format", *formatArg),
-	)
-
-	options := types.WrapperOptions{
-		Format:    *formatArg,
-		Landscape: *landscapeArg,
-		Grid: types.GridOptions{
-			Enabled: *gridArg,
-			Opacity: 0.1,
-			Color:   "black!30",
-			Scale:   1,
-		},
-		Scale: 0.9,
-		Pages: "-",
-		Delta: types.DeltaOptions{
-			X: "10",
-			Y: "10",
-		},
-		Filename: fileArg,
+	if !fs.ValidPath(filehandle) {
+		logger.Fatal("Not a valid path", zap.String("path", filehandle))
+		return 1
 	}
 
-	resPath, err := runner.Run(&options)
+	f, err := os.Stat(filehandle)
+	if err != nil {
+		logger.Fatal("Failed to stat file", zap.String("file", filehandle))
+		return 1
+	}
+
+	config, err := makeWrapperOptions(filehandle)
+
 	if err != nil {
 		logger.Fatal(err.Error())
+		return 1
 	}
 
-	logger.Info("Wrote pdf to filesystem", zap.String("path", resPath))
+	if watchArg {
+		if !f.IsDir() {
+			logger.Fatal("Given path is not a directory", zap.String("path", filehandle))
+			return 1
+		}
+		_, err := watchDirectory(filehandle)
+		if err != nil {
+			logger.Fatal(err.Error())
+			return 1
+		}
+	} else {
+		if !f.Mode().IsRegular() {
+			logger.Fatal("Given file is not regular", zap.String("file", filehandle))
+			return 1
+		}
+		output, err := oneOffBuild(filehandle, config)
+		if err != nil {
+			logger.Fatal(err.Error())
+		}
+		logger.Info(output)
+	}
+	return 0
+}
+
+func makeWrapperOptions(arg string) (*types.WrapperOptions, error) {
+	var options types.WrapperOptions
+
+	if watchArg && len(fileArg) > 0 {
+		return nil, errors.New("cannot specify --watch and --file")
+	}
+
+	file := fileArg
+	if file != "" && len(arg) > 0 {
+		return nil, errors.New("ambiguous files, specify either --file or the argument")
+	}
+	if file == "" {
+		file = arg
+	}
+
+	options.Filename = file
+
+	delta := [2]string{}
+	if len(deltaArg) == 1 {
+		delta = [2]string{delta[0], delta[0]}
+	}
+	if len(delta) != 2 {
+		return nil, errors.New("illegal dimensions of delta argument")
+	}
+
+	options.Delta.X = delta[0]
+	options.Delta.Y = delta[1]
+
+	options.Format = validateFormat(formatArg)
+
+	options.Grid.Enabled = gridArg
+	options.Grid.Color = gridColorArg
+	options.Grid.Scale = gridScaleArg
+	options.Grid.Opacity = gridOpacityArg
+
+	options.Landscape = landscapeArg
+	options.Pages = pagesArg
+	options.Scale = scaleArg
+
+	return &options, nil
+}
+
+func watchDirectory(dir string) (int, error) {
+	return 0, nil
+}
+
+func oneOffBuild(file string, config *types.WrapperOptions) (string, error) {
+	return runner.Run(config)
+}
+
+func validateFormat(format string) string {
+	for _, element := range supportedFormats {
+		if element == format {
+			return format
+		}
+	}
+	return "a3paper"
 }
