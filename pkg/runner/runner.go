bab6e71e013fafc04580b1f3de16778eb8db8edd
Alexander Bartolomey <occloxium@gmail.com>
2021-07-03 13:10:56 +0200

    feat: draft runner package
    Signed-off-by: Alexander Bartolomey <occloxium@gmail.com>

--- pkg/runner/runner.go
+++ pkg/runner/runner.go
@@ -1,13 +1,62 @@
 package runner
 
 import (
+	"os"
+	"os/exec"
+	"path/filepath"
+
+	"github.com/google/uuid"
+	template "github.com/zoomoid/papeer/pkg/template"
 	types "github.com/zoomoid/papeer/pkg/types"
 )
 
+// Run creates a temporary directory and runs the templater to write to the file before calling latexmk on the file
+//
 func Run(options *types.WrapperOptions) (string, error) {
-	return "", nil
-}
+	dir, err := os.MkdirTemp("", "runner-*")
+	if err != nil {
+		return "", err
+	}
 
-func createTempTexFile(uuid string) {
+	defer os.RemoveAll(dir)
 
+	filename := uuid.New().String()
+
+	contents, err := template.Template(options)
+
+	if err != nil {
+		return "", err
+	}
+
+	fname := filepath.Join(dir, filename+".tex")
+
+	err = os.WriteFile(fname, []byte(contents), 0644)
+
+	if err != nil {
+		return "", err
+	}
+
+	cmd := exec.Command("latexmk", "-interaction=nonstopmode", "-pdf", "-shell-escape", "-outdir"+dir, "-synctex=0", "-file-line-error", fname)
+	err = cmd.Run()
+
+	if err != nil {
+		return "", err
+	}
+
+	pwd, err := os.Getwd()
+
+	if err != nil {
+		return "", err
+	}
+
+	tempOutputFilename := filepath.Join(dir, filename+".pdf")
+	outputFilename := filepath.Join(pwd, filename+".pdf")
+
+	err = os.Rename(tempOutputFilename, outputFilename)
+
+	if err != nil {
+		return "", err
+	}
+
+	return outputFilename, nil
 }
